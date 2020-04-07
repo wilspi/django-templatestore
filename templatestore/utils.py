9c68ae2a44fcd4418161a056ff4e5c117580dc68
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-04-08 00:47:28 +0530

    minor change in base64 encoding and decoding

--- templatestore/utils.py
+++ templatestore/utils.py
@@ -2,10 +2,10 @@ import base64
 
 
 def base64decode(template):
-    decoded_bytes = base64.b64decode(template)
-    return str(decoded_bytes, "utf-8")
+    output = base64.b64decode(template.encode("utf-8"))
+    return str(output.decode("utf-8"))
 
 
 def base64encode(rendered_output):
-    encoded_bytes = base64.b64encode(rendered_output.encode("utf-8"))
-    return str(encoded_bytes, "utf-8")
+    output = base64.b64encode(rendered_output.encode("utf-8"))
+    return str(output.decode("utf-8"))
