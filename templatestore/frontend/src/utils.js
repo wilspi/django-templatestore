d4c61144807f77659b45c8659181833fbb315fc5
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-04-17 03:14:35 +0530

    fixes

--- templatestore/frontend/src/utils.js
+++ templatestore/frontend/src/utils.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
 export function encode(str) {
-    // First we escape the string using encodeURIComponent to get the UTF-8 encoding of the characters,
-    // then we convert the percent encodings into raw bytes, and finally feed it to btoa() function.
-    let utf8Bytes = encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-        return String.fromCharCode('0x' + p1);
-    });
-    console.log("encode", str, btoa(utf8Bytes));
-    return btoa(utf8Bytes);
+    var encodedString = btoa(str);
+    return encodedString;
 }
 
 export function decode(str) {
-    // Convert Base64 encoded bytes to percent-encoding, and then get the original string.
-    let percentEncodedStr = atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join('');
-    console.log("Decode", str, percentEncodedStr);
-    return decodeURIComponent(percentEncodedStr);
+    var decodedString = atob(str);
+    return decodedString;
 }
