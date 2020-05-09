c165fefcbf259cc16c501936b56356cfc9145839
Sandeep R <SandeepR@ACKO616.local>
2020-05-09 08:57:32 +0530

    production mode set in production webpack  config file

--- templatestore/frontend/webpack.prod.js
+++ templatestore/frontend/webpack.prod.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
+    mode: 'production',
     devtool: 'source-map',
 })
