30ac3d83bd2caec46ff56954cb2d8cc5aa3a4bf4
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-05-15 03:33:08 +0530

    Validation: Missing mandatory attributes

--- templatestore/views.py
+++ templatestore/views.py
@@ -553,11 +553,26 @@ def patch_attributes_view(request, name):
     if request.method == "PATCH":
         try:
             data = json.loads(request.body)
+
             if "attributes" not in data:
                 raise (Exception("Validation: Attributes are not provided"))
+
             template = Template.objects.filter(name=name)
-            if len(template) == 0:
+            if not len(template):
                 raise (Exception("Validation: Template with given name does not exist"))
+
+            missing_attributes = set(
+                ts_settings.TE_TEMPLATE_ATTRIBUTES_KEYS
+            ).difference(set(data.keys()))
+            if len(missing_attributes):
+                raise (
+                    Exception(
+                        "Validation: missing mandatory attributes `"
+                        + str(missing_attributes)
+                        + "`"
+                    )
+                )
+
             template.update(attributes=data["attributes"])
             template = Template.objects.get(name=name)
             data = {"name": name, "attributes": template.attributes}
