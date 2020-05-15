1ff9c88a0ae65d2b2350579039c717ceede27eb6
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-05-15 12:50:47 +0530

    Revert "minor change"
    This reverts commit ed83c90229b6a04086fb6d79b3a757151407dec8.

--- templatestore/frontend/src/pages/templateScreen.js
+++ templatestore/frontend/src/pages/templateScreen.js
@@ -286,7 +286,7 @@ class TemplateScreen extends Component {
             });
         if (renderMode === 'html') {
             this.setState({
-                previewSubType: subType
+                previewType: subType
             });
         }
     }
@@ -804,13 +804,13 @@ class TemplateScreen extends Component {
                                 style={{ height: '90vh', padding: '0' }}
                             >
                                 {
-                                    this.state.previewSubType ? (
+                                    this.state.previewType ? (
                                         <iframe
                                             height="100%"
                                             width="100%"
                                             srcDoc={
                                                 this.state.subTemplatesData[
-                                                    this.state.previewSubType
+                                                    this.state.previewType
                                                 ].output
                                             }
                                         />
