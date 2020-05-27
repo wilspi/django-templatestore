3c7fcfb2b328e1282c4d352d6d58e4ad2fc3bc38
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-05-28 02:02:15 +0530

    added attributes charfield in TemplateConfig model

--- templatestore/models.py
+++ templatestore/models.py
@@ -78,6 +78,7 @@ class TemplateConfig(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
     deleted_on = models.DateTimeField(null=True, blank=True)
+    attributes = models.CharField(default='{}', max_length=1000)
 
     class Meta:
         db_table = "templatestore_template_config"
