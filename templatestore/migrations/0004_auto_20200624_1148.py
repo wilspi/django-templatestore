db0d1fdf99705223832c46241bc3d65d8dc6bb8d
Vijay-Giri <lit2016038@iiitl.ac.in>
2020-06-24 17:29:27 +0530

    black formatting

--- templatestore/migrations/0004_auto_20200624_1148.py
+++ templatestore/migrations/0004_auto_20200624_1148.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('templatestore', '0003_templateversion_version_alias'),
+        ("templatestore", "0003_templateversion_version_alias"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='template',
-            name='created_by',
+            model_name="template",
+            name="created_by",
             field=models.IntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='templateversion',
-            name='created_by',
+            model_name="templateversion",
+            name="created_by",
             field=models.IntegerField(blank=True, null=True),
         ),
     ]
