548cab3a72b59eae5f25adb74bbb0ccc6151fa0d
Naimish Rastogi <naimish.rastogi_int@acko.tech>
2022-03-01 13:42:34 +0530

    fixes for uat

--- templatestore/utils.py
+++ templatestore/utils.py
@@ -14,23 +14,28 @@ def base64encode(rendered_output):
 
 def generateDate(days):
     # .isoformat()
-    IST = pytz.timezone('Asia/Kolkata')
-    dt = datetime.datetime.now(IST)+relativedelta(days=+days)
+    IST = pytz.timezone("Asia/Kolkata")
+    dt = datetime.datetime.now(IST) + relativedelta(days=+days)
     return str(dt.isoformat())
 
-def generatePayload(templateTable,versionTable,param):
-    ans=[]
-    i=0
-    while i< len(versionTable.tiny_url):
-        original_url='versionTable.sample_context_data'+versionTable.tiny_url[i]['urlKey']
-        lob=templateTable.attributes['lob']
-        journey=templateTable.attributes['journey']
-        days=versionTable.tiny_url[i]['expiry']
-        expiry=generateDate(int(days))
-        ans.append({"original_url":eval(original_url),
-                "lob":lob,
-                "journey":journey,
-                "expiry_time":expiry
-                })
-        i=i+1
+def generatePayload(templateTable, versionTable, param):
+    ans = []
+    i = 0
+    while i < len(versionTable.tiny_url):
+        original_url = (
+            "versionTable.sample_context_data" + versionTable.tiny_url[i]["urlKey"]
+        )
+        lob = templateTable.attributes["lob"]
+        journey = templateTable.attributes["journey"]
+        days = versionTable.tiny_url[i]["expiry"]
+        expiry = generateDate(int(days))
+        ans.append(
+            {
+                "original_url": eval(original_url),
+                "lob": lob,
+                "journey": journey,
+                "expiry_time": expiry,
+            }
+        )
+        i = i + 1
     return ans
