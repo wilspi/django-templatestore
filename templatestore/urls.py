aa1acce768f2daf065c9bf1f89154b0ccf9a7ff0
Rishabh Jha <rishabh.jha@ACKO1406.local>
2024-01-18 13:58:48 +0530

    Added internal endpoint for create template

--- templatestore/urls.py
+++ templatestore/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("api/v1/templates", views.get_templates_view),
     path("api/v2/templates", views.get_templates_view_v2),
     path("api/v1/tiny_url", views.save_tiny_url),
+    path("internal/api/v1/template", views.post_template_view),
     re_path("api/v1/tiny_url/(?P<name>[a-zA-Z]+[a-zA-Z0-9_]*)/(?P<version>\d+\.\d+)$", views.get_tiny_url),
     re_path(
         "api/v1/template/(?P<name>[a-zA-Z]+[a-zA-Z0-9_]*)/versions$",
